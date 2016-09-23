1fd0e390bc7816b9960073b464f9f6bf2607890a
Brian Schaefer <physinet@gmail.com>
2016-09-23 12:49:03 -0400

    Actually use the manifest

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='PyANC350',
       description='Python Driver for Attocube ANC350',
-      packages=['pyanc350v4']
+      packages=['pyanc350v4'],
+      include_package_data=True
      )
